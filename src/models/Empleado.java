7174db97095ef4b45196012cf7b5e12ee3f1ac4b
Josue Jair Gil Dolores <jgild490@alumno.uaemex.mx>
2021-02-24 13:05:46 -0600

    Creacion de Clases con Herencia

---
+++ src/models/Empleado.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author jair
+ */
+public class Empleado extends Persona {
+    
+    private String noTelefono;
+    private String correoElectronico;
+    private String direccion;
+    private float salario;
+
+    public String getNoTelefono() {
+        return noTelefono;
+    }
+
+    public void setNoTelefono(String noTelefono) {
+        this.noTelefono = noTelefono;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+    
+}
