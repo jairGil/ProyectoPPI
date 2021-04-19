95f3f9d3f109cc6faf60c4b76004d764b486d186
jairGil <salasun.jg@gmail.com>
2021-04-19 11:14:17 -0500

    Revert "Revert "Revert "Agrega Excepciones"""
    This reverts commit b15b42b724beec718640cc0d02e299df38b82694.

---
+++ src/models/Gerente.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author jair
+ */
+public class Gerente extends Empleado{
+
+    public Gerente(String noTelefono, String correoElectronico, String direccion, 
+            float salario, String nombre, String apellidoPaterno, String apellidoMaterno) {
+        super(noTelefono, correoElectronico, direccion, salario, nombre, apellidoPaterno, apellidoMaterno);
+    }
+
+    public Gerente(String noTelefono, float salario, String nombre, 
+            String apellidoPaterno, String apellidoMaterno) {
+        super(noTelefono, salario, nombre, apellidoPaterno, apellidoMaterno);
+    }
+
+    public Gerente(String noTelefono, String correoElectronico, float salario, 
+            String nombre, String apellidoPaterno, String apellidoMaterno) {
+        super(noTelefono, correoElectronico, salario, nombre, apellidoPaterno, apellidoMaterno);
+    }
+
+    public Gerente(float salario, String nombre, String apellidoPaterno, 
+            String apellidoMaterno) {
+        super(salario, nombre, apellidoPaterno, apellidoMaterno);
+    }
+    
+}
