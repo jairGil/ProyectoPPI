71681580547335182cc3cd00d1e9f008fb51fedb
jairGil <salasun.jg@gmail.com>
2021-04-19 11:19:28 -0500

    clases en paquetes

---
+++ src/excepciones/ExcepcionesString.java
@@ -0,0 +1,24 @@
+package excepciones;
+
+public class ExcepcionesString extends Exception {
+
+    public ExcepcionesString() {
+    }
+
+    public ExcepcionesString(String string) {
+        super(string);
+    }
+
+    public ExcepcionesString(String string, Throwable thrwbl) {
+        super(string, thrwbl);
+    }
+
+    public ExcepcionesString(Throwable thrwbl) {
+        super(thrwbl);
+    }
+
+    public ExcepcionesString(String string, Throwable thrwbl, boolean bln, boolean bln1) {
+        super(string, thrwbl, bln, bln1);
+    }
+    
+}
