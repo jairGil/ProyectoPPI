af28b6451ec8e16789f53dce747d697ec441cbda
jairGil <salasun.jg@gmail.com>
2021-04-19 11:13:22 -0500

    Revert "Agrega Excepciones"
    This reverts commit 8b32da3ae4777b27f11bc102c5bb5d37137b4712.

---
+++ src/models/Persona.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author jair
+ */
+public class Persona {
+
+    private String nombre;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    
+    public Persona(String nombre, String apellidoPaterno, String apellidoMaterno) {
+        this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+    
+}
