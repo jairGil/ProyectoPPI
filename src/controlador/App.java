7e4f16e69e174c1586b84c20bc622e7fe349e39a
jairGil <salasun.jg@gmail.com>
2021-05-05 19:05:31 -0500

    Clase principal de control de ventana de inicio de sesion

---
+++ src/controlador/App.java
@@ -0,0 +1,11 @@
+package controlador;
+
+import vista.FrmInicio;
+
+public class App {
+    
+    public static void main(String[] args) {
+        FrmInicio app = new FrmInicio();
+        app.setVisible(true);
+    }
+}
