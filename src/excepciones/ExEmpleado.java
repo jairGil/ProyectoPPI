768614d3b6c71b779d0c76c254eae46c0f9f49aa
jairGil <salasun.jg@gmail.com>
2021-05-05 19:04:58 -0500

    Modificacion de nombres y constructor de excepciones y agregado de ExEmpleado

---
+++ src/excepciones/ExEmpleado.java
@@ -0,0 +1,12 @@
+package excepciones;
+
+public class ExEmpleado extends Exception {
+
+    public ExEmpleado() {
+    }
+
+    public ExEmpleado(String string) {
+        super(string);
+    }
+    
+}
---
+++ src/excepciones/ExPersona.java
@@ -0,0 +1,12 @@
+package excepciones;
+
+public class ExPersona extends Exception {
+
+    public ExPersona() {
+    }
+
+    public ExPersona(String string) {
+        super(string);
+    }
+    
+}
