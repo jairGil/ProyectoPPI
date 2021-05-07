61d816e679c9455c310cf2b0c24a399934e42a38
jairGil <salasun.jg@gmail.com>
2021-05-06 20:45:55 -0500

    Nueva excepcion

---
+++ src/excepciones/ExHorario.java
@@ -0,0 +1,12 @@
+package excepciones;
+
+public class ExHorario extends Exception {
+
+    public ExHorario() {
+    }
+
+    public ExHorario(String string) {
+        super(string);
+    }
+    
+}
