9e94d4af37dc4f076408b715ae664d686f8a6df8
Danorpx <>
2021-05-11 14:11:30 -0500

    Clase Asiento

--- src/modelos/Asiento.java
+++ src/modelos/Asiento.java
@@ -1,5 +1,37 @@
 package modelos;
 
+import excepciones.ExAsiento;
+
 public class Asiento {
+    
+    private char fila;
+    private int columna;
+    private boolean ocupado;
+
+    public Asiento(char fila, int columna, boolean ocupado) throws ExAsiento {
+        setFila(fila);
+        setColumna(columna);
+        this.ocupado = ocupado;
+    }
+    
+    public void ocuparAsiento(){
+        this.ocupado = true;
+    }
+
+    public void setFila(char fila) throws ExAsiento {
+        if(fila < 65 && fila > 90)
+            throw new ExAsiento("Valor de columnas inválido");
+        if(fila < 97 && fila > 122)
+            throw new ExAsiento("Valor de columnas inválido");
+        this.fila = fila;
+    }
+
+    public void setColumna(int columna) throws ExAsiento {
+        if(columna <= 0)
+            throw new ExAsiento("Número de columnas inválido");
+        this.columna = columna;
+    }
+    
+    
     
 }
