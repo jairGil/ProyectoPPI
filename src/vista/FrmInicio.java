bd5cf8605b6606c9b2effb5c83683ef556417e2e
jairGil <salasun.jg@gmail.com>
2021-05-31 10:05:24 -0500

    Colores nuevos y pequeñas modificaciones

--- src/vista/FrmInicio.java
+++ src/vista/FrmInicio.java
@@ -2,15 +2,19 @@ package vista;
 
 public class FrmInicio extends javax.swing.JFrame {
     
-    private static final java.awt.Color ROJO = new java.awt.Color(204, 0, 0);
+    private static final java.awt.Color ROJO = new java.awt.Color(200, 0, 0);
+    private static final java.awt.Color ROJO_OBS = new java.awt.Color(150, 0, 0);
     private static final java.awt.Color GRIS = new java.awt.Color(102, 102, 102);
+    private static final java.awt.Color FG_COLOR = new java.awt.Color(255, 255, 255);
     private static final javax.swing.border.MatteBorder BORDE_ROJO = javax.swing.BorderFactory.createMatteBorder(0, 0, 2, 0, ROJO);
     private static final javax.swing.border.MatteBorder BORDE_GRIS = javax.swing.BorderFactory.createMatteBorder(0, 0, 1, 0, GRIS);
-
+    private static final java.awt.Font FUENTE_LBL = new java.awt.Font("Yu Gothic UI", 0, 18);
+    private static final java.awt.Font FUENTE_TXT = new java.awt.Font("Yu Gothic UI Light", 0, 14);
+    
     public FrmInicio() {
         initComponents();
     }
-
+   
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -21,7 +25,8 @@ public class FrmInicio extends javax.swing.JFrame {
         txtUsuario = new javax.swing.JTextField();
         lblContrasena = new javax.swing.JLabel();
         txtContrasena = new javax.swing.JPasswordField();
-        lblIngresar = new javax.swing.JLabel();
+        btnIngresar = new javax.swing.JButton();
+        lblRegistro = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Cinema");
@@ -34,14 +39,16 @@ public class FrmInicio extends javax.swing.JFrame {
         lblImgUsuario.setIcon(new javax.swing.ImageIcon(getClass().getResource("/iconos/user.png"))); // NOI18N
 
         lblUsuario.setBackground(new java.awt.Color(204, 204, 204));
-        lblUsuario.setFont(new java.awt.Font("Yu Gothic UI", 0, 18)); // NOI18N
-        lblUsuario.setForeground(new java.awt.Color(255, 255, 255));
+        lblUsuario.setFont(FUENTE_LBL);
+        lblUsuario.setForeground(FG_COLOR);
         lblUsuario.setText("Usuario");
 
         txtUsuario.setBackground(new java.awt.Color(51, 51, 51));
-        txtUsuario.setFont(new java.awt.Font("Yu Gothic UI Light", 0, 14)); // NOI18N
+        txtUsuario.setFont(FUENTE_TXT);
         txtUsuario.setForeground(new java.awt.Color(204, 204, 204));
         txtUsuario.setBorder(BORDE_GRIS);
+        txtUsuario.setCursor(new java.awt.Cursor(java.awt.Cursor.TEXT_CURSOR));
+        txtUsuario.setDoubleBuffered(true);
         txtUsuario.addFocusListener(new java.awt.event.FocusAdapter() {
             public void focusGained(java.awt.event.FocusEvent evt) {
                 txtUsuarioFocusGained(evt);
@@ -52,12 +59,12 @@ public class FrmInicio extends javax.swing.JFrame {
         });
 
         lblContrasena.setBackground(new java.awt.Color(204, 204, 204));
-        lblContrasena.setFont(new java.awt.Font("Yu Gothic UI", 0, 18)); // NOI18N
-        lblContrasena.setForeground(new java.awt.Color(255, 255, 255));
+        lblContrasena.setFont(FUENTE_LBL);
+        lblContrasena.setForeground(FG_COLOR);
         lblContrasena.setText("Contraseña");
 
         txtContrasena.setBackground(new java.awt.Color(51, 51, 51));
-        txtContrasena.setFont(new java.awt.Font("Yu Gothic Light", 0, 14)); // NOI18N
+        txtContrasena.setFont(FUENTE_TXT);
         txtContrasena.setForeground(new java.awt.Color(204, 204, 204));
         txtContrasena.setBorder(BORDE_GRIS);
         txtContrasena.addFocusListener(new java.awt.event.FocusAdapter() {
@@ -69,15 +76,39 @@ public class FrmInicio extends javax.swing.JFrame {
             }
         });
 
-        lblIngresar.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        lblIngresar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/iconos/login_claro.png"))); // NOI18N
-        lblIngresar.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        lblIngresar.addMouseListener(new java.awt.event.MouseAdapter() {
+        btnIngresar.setBackground(ROJO);
+        btnIngresar.setFont(FUENTE_LBL);
+        btnIngresar.setForeground(FG_COLOR);
+        btnIngresar.setText("Ingresar");
+        btnIngresar.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 1, 1, 1, ROJO_OBS));
+        btnIngresar.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnIngresar.setFocusPainted(false);
+        btnIngresar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btnIngresarMouseClicked(evt);
+            }
             public void mouseEntered(java.awt.event.MouseEvent evt) {
-                lblIngresarMouseEntered(evt);
+                btnIngresarMouseEntered(evt);
             }
             public void mouseExited(java.awt.event.MouseEvent evt) {
-                lblIngresarMouseExited(evt);
+                btnIngresarMouseExited(evt);
+            }
+        });
+
+        lblRegistro.setFont(new java.awt.Font("Yu Gothic UI", 1, 18)); // NOI18N
+        lblRegistro.setForeground(GRIS);
+        lblRegistro.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblRegistro.setText("Registrarse");
+        lblRegistro.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        lblRegistro.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                lblRegistroMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                lblRegistroMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                lblRegistroMouseExited(evt);
             }
         });
 
@@ -85,7 +116,7 @@ public class FrmInicio extends javax.swing.JFrame {
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+            .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(40, 40, 40)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(lblImgUsuario, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -96,7 +127,9 @@ public class FrmInicio extends javax.swing.JFrame {
                 .addGap(40, 40, 40))
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(lblIngresar)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(btnIngresar, javax.swing.GroupLayout.DEFAULT_SIZE, 118, Short.MAX_VALUE)
+                    .addComponent(lblRegistro, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
@@ -106,15 +139,17 @@ public class FrmInicio extends javax.swing.JFrame {
                 .addComponent(lblImgUsuario)
                 .addGap(27, 27, 27)
                 .addComponent(lblUsuario)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(txtUsuario, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(32, 32, 32)
+                .addGap(30, 30, 30)
                 .addComponent(lblContrasena)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(txtContrasena, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 50, Short.MAX_VALUE)
-                .addComponent(lblIngresar, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(20, 20, 20))
+                .addGap(50, 50, 50)
+                .addComponent(btnIngresar, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(lblRegistro)
+                .addContainerGap(30, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -125,20 +160,12 @@ public class FrmInicio extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
 
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
-
-    private void lblIngresarMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblIngresarMouseEntered
-        lblIngresar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/iconos/login_rojo.png")));
-    }//GEN-LAST:event_lblIngresarMouseEntered
-
-    private void lblIngresarMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblIngresarMouseExited
-        lblIngresar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/iconos/login_claro.png")));
-    }//GEN-LAST:event_lblIngresarMouseExited
 
     private void txtUsuarioFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_txtUsuarioFocusGained
         txtUsuario.setBorder(BORDE_ROJO);
@@ -156,11 +183,36 @@ public class FrmInicio extends javax.swing.JFrame {
         txtContrasena.setBorder(BORDE_GRIS);
     }//GEN-LAST:event_txtContrasenaFocusLost
 
+    private void lblRegistroMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblRegistroMouseEntered
+        lblRegistro.setForeground(ROJO);
+    }//GEN-LAST:event_lblRegistroMouseEntered
+
+    private void lblRegistroMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblRegistroMouseExited
+        lblRegistro.setForeground(GRIS);
+    }//GEN-LAST:event_lblRegistroMouseExited
+
+    private void btnIngresarMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnIngresarMouseEntered
+        btnIngresar.setBackground(ROJO_OBS);
+    }//GEN-LAST:event_btnIngresarMouseEntered
+
+    private void btnIngresarMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnIngresarMouseExited
+        btnIngresar.setBackground(ROJO);
+    }//GEN-LAST:event_btnIngresarMouseExited
+
+    private void btnIngresarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnIngresarMouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnIngresarMouseClicked
+
+    private void lblRegistroMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblRegistroMouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_lblRegistroMouseClicked
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnIngresar;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JLabel lblContrasena;
     private javax.swing.JLabel lblImgUsuario;
-    private javax.swing.JLabel lblIngresar;
+    private javax.swing.JLabel lblRegistro;
     private javax.swing.JLabel lblUsuario;
     private javax.swing.JPasswordField txtContrasena;
     private javax.swing.JTextField txtUsuario;
