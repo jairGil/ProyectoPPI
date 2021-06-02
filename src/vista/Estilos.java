30a236863e8a459a714d2b6577da8a59e43f4c1b
jairGil <salasun.jg@gmail.com>
2021-06-02 10:46:15 -0500

    Agregado de fuente de titulo

--- src/vista/Estilos.java
+++ src/vista/Estilos.java
@@ -9,10 +9,12 @@ public class Estilos {
     public static final java.awt.Color GRIS_BG = new java.awt.Color(51, 51, 51);
     public static final java.awt.Color ROJO = new java.awt.Color(200, 0, 0);
     public static final java.awt.Color ROJO_OBS = new java.awt.Color(150, 0, 0);
+    public static final java.awt.Color GRIS_SEL = new java.awt.Color(35, 35, 35);
     
     // Fuentes y color de fuente
     public static final java.awt.Color FG_COLOR = new java.awt.Color(255, 255, 255);
     public static final java.awt.Color GRIS_CLARO = new java.awt.Color(102, 102, 102);
+    public static final java.awt.Font FUENTE_TITULOS = new java.awt.Font("Yu Gothic UI", 0, 28);
     public static final java.awt.Font FUENTE_LBL = new java.awt.Font("Yu Gothic UI", 0, 18);
     public static final java.awt.Font FUENTE_TXT = new java.awt.Font("Yu Gothic UI Light", 0, 14);
     
