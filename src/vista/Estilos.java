07ddd4c0aba0428ad03ff0773c51548950b6e2fb
jairGil <salasun.jg@gmail.com>
2021-06-02 09:51:10 -0500

    Clase de estilos para estandar de interfaz

---
+++ src/vista/Estilos.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vista;
+
+/**
+ *
+ * @author salas
+ */
+public class Estilos {
+    public static final java.awt.Color ROJO = new java.awt.Color(200, 0, 0);
+    public static final java.awt.Color ROJO_OBS = new java.awt.Color(150, 0, 0);
+    public static final java.awt.Color GRIS = new java.awt.Color(102, 102, 102);
+    public static final java.awt.Color FG_COLOR = new java.awt.Color(255, 255, 255);
+    public static final javax.swing.border.MatteBorder BORDE_ROJO = javax.swing.BorderFactory.createMatteBorder(0, 0, 2, 0, ROJO);
+    public static final javax.swing.border.MatteBorder BORDE_GRIS = javax.swing.BorderFactory.createMatteBorder(0, 0, 1, 0, GRIS);
+    public static final java.awt.Font FUENTE_LBL = new java.awt.Font("Yu Gothic UI", 0, 18);
+    public static final java.awt.Font FUENTE_TXT = new java.awt.Font("Yu Gothic UI Light", 0, 14);
+}
