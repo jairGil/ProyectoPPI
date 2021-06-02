dc3284e95b779439620e32caabaa14c2f19ec6a1
jairGil <salasun.jg@gmail.com>
2021-06-02 09:38:50 -0500

    Corrección de eroores

--- src/controlador/crud/DaoGen.java
+++ src/controlador/crud/DaoGen.java
@@ -10,24 +10,30 @@ import java.util.ArrayList;
 /**
  *
  * @author Dell
+ * @param <T>
  */
-public class DaoGen<T> implements IGen{
+public class DaoGen<T> implements IGen<T> {
     ArrayList<T> lista = new ArrayList();
+    
+    @Override
     public void crearT(T o,String n) {
         lista = Archivos.leerObjeto(n);
         lista.add(o);
         Archivos.escribirObjeto(lista,n);
     }
     
+    @Override
     public ArrayList<T> leerT(String n) {
         lista = Archivos.leerObjeto(n);
         return lista;
     }  
     
+    @Override
     public void actualizarT(ArrayList<T> o, String n) {
         Archivos.escribirObjeto(o,n);
     }
     
+    @Override
     public void eliminarT(ArrayList<T> ol, T oe){
        
     }
