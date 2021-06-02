1ee5d6f65ab33ecf233219abfdf20874c3d2d33b
jairGil <salasun.jg@gmail.com>
2021-06-02 10:45:51 -0500

    Estilos del frame principal

---
+++ src/vista/FrmPrincipal.java
@@ -0,0 +1,134 @@
+package vista;
+
+import javax.swing.UIManager;
+import static vista.Estilos.*;
+
+public class FrmPrincipal extends javax.swing.JFrame {
+
+    public FrmPrincipal() {
+        initEstilos();
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jSplitPane1 = new javax.swing.JSplitPane();
+        PnlIzquierdo = new javax.swing.JPanel();
+        jToggleButton1 = new javax.swing.JToggleButton();
+        PnlDerecho = new javax.swing.JPanel();
+        PnlEncabezado = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setLocation(new java.awt.Point(0, 0));
+        setLocationByPlatform(true);
+
+        jSplitPane1.setBorder(null);
+        jSplitPane1.setDividerLocation(200);
+        jSplitPane1.setDividerSize(1);
+        jSplitPane1.setContinuousLayout(true);
+
+        PnlIzquierdo.setBackground(GRIS_BG);
+
+        jToggleButton1.setText("INICIO");
+        jToggleButton1.setBorder(null);
+        jToggleButton1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jToggleButton1.setFocusPainted(false);
+
+        javax.swing.GroupLayout PnlIzquierdoLayout = new javax.swing.GroupLayout(PnlIzquierdo);
+        PnlIzquierdo.setLayout(PnlIzquierdoLayout);
+        PnlIzquierdoLayout.setHorizontalGroup(
+            PnlIzquierdoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jToggleButton1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 200, Short.MAX_VALUE)
+        );
+        PnlIzquierdoLayout.setVerticalGroup(
+            PnlIzquierdoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(PnlIzquierdoLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jToggleButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(283, Short.MAX_VALUE))
+        );
+
+        jSplitPane1.setLeftComponent(PnlIzquierdo);
+
+        PnlDerecho.setBackground(ROJO_OBS);
+
+        javax.swing.GroupLayout PnlDerechoLayout = new javax.swing.GroupLayout(PnlDerecho);
+        PnlDerecho.setLayout(PnlDerechoLayout);
+        PnlDerechoLayout.setHorizontalGroup(
+            PnlDerechoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 528, Short.MAX_VALUE)
+        );
+        PnlDerechoLayout.setVerticalGroup(
+            PnlDerechoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 334, Short.MAX_VALUE)
+        );
+
+        jSplitPane1.setRightComponent(PnlDerecho);
+
+        PnlEncabezado.setBackground(GRIS_BG);
+
+        jLabel1.setFont(FUENTE_TITULOS);
+        jLabel1.setForeground(FG_COLOR);
+        jLabel1.setText("Gestión de Cine");
+
+        javax.swing.GroupLayout PnlEncabezadoLayout = new javax.swing.GroupLayout(PnlEncabezado);
+        PnlEncabezado.setLayout(PnlEncabezadoLayout);
+        PnlEncabezadoLayout.setHorizontalGroup(
+            PnlEncabezadoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(PnlEncabezadoLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 430, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        PnlEncabezadoLayout.setVerticalGroup(
+            PnlEncabezadoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 49, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jSplitPane1)
+            .addComponent(PnlEncabezado, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addComponent(PnlEncabezado, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, 0)
+                .addComponent(jSplitPane1))
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    /**
+     * @param args the command line arguments
+     * 
+     */
+    public static void main(String args[]) {
+        new FrmPrincipal().setVisible(true);
+    }
+    
+    private void initEstilos() {
+        UIManager.put("ToggleButton.font", FUENTE_LBL);
+        UIManager.put("ToggleButton.foreground", FG_COLOR);
+        UIManager.put("ToggleButton.background", GRIS_BG);
+        UIManager.put("ToggleButton.select", GRIS_SEL);
+        UIManager.put("ToggleButton.focuspainted", false);
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel PnlDerecho;
+    private javax.swing.JPanel PnlEncabezado;
+    private javax.swing.JPanel PnlIzquierdo;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JSplitPane jSplitPane1;
+    private javax.swing.JToggleButton jToggleButton1;
+    // End of variables declaration//GEN-END:variables
+}
